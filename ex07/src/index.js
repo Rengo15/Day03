aa347a24c5f4209faade09025b11aa6585f79814
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-11 00:53:43 +0100

    Created ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,30 @@
+// Variable declarations
+var StUdLyCapVaR;
+var properCamelCase;
+var TitleCaseOver;
+
+// Variable assignments
+STUDLYCAPVAR = 10;
+PRoperCAmelCAse = "A String";
+tITLEcASEoVER = 9000;
+
+function main() {
+    // Only change code below this line
+    // Variable declarations
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+
+    // Variable assignments
+    var studlyCapVar = STUDLYCAPVAR;
+    var properCamelCase = PRoperCAmelCAse;
+    var titleCaseOver = tITLEcASEoVER;
+    // Only change code above this line
+    return {
+        studlyCapVar,
+        properCamelCase,
+        titleCaseOver
+    }    
+}
+console.log(main());
+module.exports = main;
